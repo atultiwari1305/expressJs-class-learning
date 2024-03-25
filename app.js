37089b9c90db3469f4c05dbb2d555137df457374
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-03-25 10:52:28 +0530

    expressJS basics'

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 //     console.log("Express in Working Always");
 // })
 
+
+
 // //whenever more than one response is used we need to use next callback function to execute other responses
 // app.use((req,res,next)=>{
 //     console.log("January");
@@ -28,13 +30,34 @@ const app = express();
 //     // next();
 // })
 
-//writing in browser
-app.use('/second',(req,res,next)=>{
-    res.send('<h1>Happy Learning Day: 1</h1><br>');
-    next();
+
+
+// //writing in browser
+// app.use('/second',(req,res,next)=>{
+//     res.send('<h1>Happy Learning Day: 1</h1><br>');
+//     next();
+// })
+// app.use('/',(req,res,next)=>{
+//     res.send('<h2>Happy Learning Day: 2</h2>');
+// })
+
+
+
+//Different URL and Different Route
+
+app.use('/third',(req,res)=>{
+    console.log('Third Happy');
+    res.send('<h1>Happy Happy Happy</h1>');
 })
-app.use('/',(req,res,next)=>{
-    res.send('<h2>Happy Learning Day: 2</h2>');
+
+app.use('/second',(req,res)=>{
+    console.log('Second Happy');
+    res.send('<h1>Happy Happy</h1>');
+})
+
+app.use('/',(req,res)=>{
+    console.log('First Happy');
+    res.send('<h1>Happy</h1>');
 })
 
 const server = http.createServer(app);
